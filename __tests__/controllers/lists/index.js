a02d70e7dc6f963b1dfd4cd252a37977e51208e9
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-02-21 19:06:58 +0100

    chore(__tests__/controllers/lists/index.js) tests improved

--- __tests__/controllers/lists/index.js
+++ __tests__/controllers/lists/index.js
@@ -10,6 +10,7 @@ describe('[POST] - /lists', () => {
         password: 'myfonciapassword',
       })
       .set('Content-Type', 'application/json');
+
     expect(signIn.statusCode).toEqual(204);
 
     const login = await request(app)
@@ -19,23 +20,42 @@ describe('[POST] - /lists', () => {
         password: 'myfonciapassword',
       })
       .set('Content-Type', 'application/json');
+
     expect(login.statusCode).toEqual(200);
     expect(login.body).toHaveProperty('token');
 
     const list = await request(app)
       .get(`/lists`)
-      .set('Content-Type', 'application/json');
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${login.body.token}`);
+
     expect(list.statusCode).toEqual(200);
-    console.log(list.body);
+    expect(list.body.results).toHaveLength(10);
     expect(list.body.pagination).toHaveProperty('current', 1);
+    expect(list.body.pagination).toHaveProperty('pageCount', 13);
+    expect(list.body.pagination).toHaveProperty('totalCount', 122);
 
     const listPage2 = await request(app)
       .get(`/lists?page=2`)
-      .set('Content-Type', 'application/json');
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${login.body.token}`);
 
     expect(listPage2.statusCode).toEqual(200);
-    console.log(listPage2.body);
+    expect(listPage2.body.results).toHaveLength(10);
     expect(listPage2.body.pagination).toHaveProperty('current', 2);
+    expect(listPage2.body.pagination).toHaveProperty('pageCount', 13);
+    expect(listPage2.body.pagination).toHaveProperty('totalCount', 122);
+
+    const listPage1000000 = await request(app)
+      .get(`/lists?page=1000000`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${login.body.token}`);
+
+    expect(listPage1000000.statusCode).toEqual(200);
+    expect(listPage1000000.body.results).toHaveLength(0);
+    expect(listPage1000000.body.pagination).toHaveProperty('current', 1000000);
+    expect(listPage1000000.body.pagination).toHaveProperty('pageCount', 13);
+    expect(listPage1000000.body.pagination).toHaveProperty('totalCount', 122);
     done();
   });
 
@@ -43,6 +63,7 @@ describe('[POST] - /lists', () => {
     const list = await request(app)
       .get(`/lists`)
       .set('Content-Type', 'application/json');
+
     expect(list.statusCode).toEqual(401);
     done();
   });
