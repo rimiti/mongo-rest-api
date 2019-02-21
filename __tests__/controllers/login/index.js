9181d25350b9dc19a3f73bb60d35702df68238ec
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-02-21 18:39:21 +0100

    feat(__tests__/controllers/login/index.js) tests created

---
+++ __tests__/controllers/login/index.js
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import app from '../../../src';
+
+describe('[POST] - /login', () => {
+  it('Should returns 200', async (done) => {
+    const signIn = await request(app)
+      .post(`/users`)
+      .send({
+        login: 'john',
+        password: 'myfonciapassword',
+      })
+      .set('Content-Type', 'application/json');
+    expect(signIn.statusCode).toEqual(204);
+    done();
+
+    const response = await request(app)
+      .post(`/login`)
+      .send({
+        login: 'john',
+        password: 'myfonciapassword',
+      })
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('token');
+    done();
+  });
+
+  it('Should returns 400', async (done) => {
+    const response = await request(app)
+      .post(`/login`)
+      .send({
+        login: 'john2',
+        password: 'wrongPassword',
+      })
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toEqual(400);
+    done();
+  });
+});
