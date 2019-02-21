79a73d4cc131e612a571b1b45428cca27c276ad8
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-02-21 12:07:35 +0100

    feat(controllers/users/index.js) file created

---
+++ src/controllers/users/index.js
@@ -0,0 +1,48 @@
+import {celebrate, Joi} from 'celebrate';
+import {UserModel} from '../../db';
+import {encrypt} from '../../common/crypto';
+import Acl, {roles} from '../../acl'
+
+/**
+ * @description Users controller.
+ */
+export default class Users {
+  /**
+   * @description POST.
+   * @param req
+   * @param res
+   * @param next
+   * @return {Promise<*>}
+   */
+  static async create(req, res, next) {
+    const {login, password} = req.body;
+
+    const exists = await UserModel.findOne({login, password: encrypt(password)});
+    if(exists !== null) {
+      return res.status(400).send();
+    }
+
+    const User = new UserModel({login, password: encrypt(password)});
+    const user = await User.save();
+    await Acl.addUserRoles(user._id, roles.gestionnaire);
+
+    // TODO: Send email confirmation with token to verify account
+    return res.status(200).send();
+  }
+
+  /**
+   * @description POST validator.
+   * @return {}
+   */
+  static createValidator() {
+    return celebrate(
+      {
+        body: {
+          login: Joi.string().email({minDomainAtoms: 2}).required(),
+          password: Joi.string().min(6).required(),
+        },
+      },
+      {abortEarly: false},
+    );
+  }
+}
