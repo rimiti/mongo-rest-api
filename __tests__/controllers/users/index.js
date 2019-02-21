693b2f60f2891abbaea1fd6aa8c8472de77c3d31
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-02-21 18:39:32 +0100

    feat(__tests__/controllers/users/index.js) tests created

---
+++ __tests__/controllers/users/index.js
@@ -0,0 +1,28 @@
+import request from 'supertest';
+import app from '../../../src';
+
+describe('[POST] - /users', () => {
+  it('Should returns 200', async (done) => {
+    const response = await request(app)
+      .post(`/users`)
+      .send({
+        login: 'dimitri',
+        password: 'mypassword',
+      })
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toEqual(204);
+    done();
+  });
+
+  it('Should returns 400 (account already exists)', async (done) => {
+    const response = await request(app)
+      .post(`/users`)
+      .send({
+        login: 'dimitri',
+        password: 'mypassword',
+      })
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toEqual(400);
+    done();
+  });
+});
